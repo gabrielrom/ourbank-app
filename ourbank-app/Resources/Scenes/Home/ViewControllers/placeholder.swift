f83efb6eae6e5cc4e8fa0866c22979376fc0315d
Gabriel Matheus <gabriel.souza@modal.com.br>
2022-02-25 21:08:03 -0300

    :recycle: refact: remove scenes in resources file and add appdelegate

--- ourbank-app/Resources/Scenes/Home/ViewControllers/placeholder.swift
+++
@@ -1,8 +0,0 @@
-//
-//  placeholder.swift
-//  ourbank-app
-//
-//  Created by GABRIEL MATHEUS RODRIGUES DE SOUZA on 13/12/21.
-//
-
-import Foundation
--- ourbank-app/Resources/Scenes/Sessions/ViewControllers/SignUpViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class SignUpViewController: UIViewController {
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-  
-}
